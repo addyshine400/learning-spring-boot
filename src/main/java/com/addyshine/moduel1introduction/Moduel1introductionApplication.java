f61c14b110373f60920a6ead9d5d023ebdc7a5ca
Aditya Raj Pandey <adityarajpandey6374@gmail.com>
2026-09-05 18:22:05 +0530

    this is fourth commit

--- src/main/java/com/addyshine/moduel1introduction/Moduel1introductionApplication.java
+++ src/main/java/com/addyshine/moduel1introduction/Moduel1introductionApplication.java
@@ -6,23 +6,31 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class  Moduel1introductionApplication implements CommandLineRunner {
 	//@Autowired
- final	NotificationService notificationServiceobj;  //DEPENDENCY INJECTION
+ //final	NotificationService notificationServiceobj;  //DEPENDENCY INJECTION
 
-	public Moduel1introductionApplication( @Qualifier("EMAILnotify") NotificationService notificationServiceobj) {
-		this.notificationServiceobj = notificationServiceobj; // constructor DI
-	}
+	//public Moduel1introductionApplication(@Qualifier("EMAILnotify") NotificationService notificationServiceobj) {
+	//	this.notificationServiceobj = notificationServiceobj; // constructor DI
+	//}
 
-
+    @Autowired
+    Map<String , NotificationService> notificationServiceMap = new HashMap<>();
 	public static void main(String[] args) {
 
 		SpringApplication.run(Moduel1introductionApplication.class, args);
 	}
 	@Override
 	public void run(String... args) throws Exception {
-		notificationServiceobj.send("Hello World");
+		//notificationServiceobj.send("Hello World");
+		for(var notificationService: notificationServiceMap.entrySet() ){
+			System.out.println(notificationService.getKey());
+			notificationService.getValue().send("hello addy!!");
+		}
 	}
 
 }
--- src/main/java/com/addyshine/moduel1introduction/impl/EmailNotificationService.java
+++ src/main/java/com/addyshine/moduel1introduction/impl/EmailNotificationService.java
@@ -2,10 +2,12 @@ package com.addyshine.moduel1introduction.impl;
 
 import com.addyshine.moduel1introduction.NotificationService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
  @Component
  @Qualifier("EMAILnotify")
+ //@ConditionalOnProperty(name = "notification.type" , havingValue = "email")
 public class EmailNotificationService implements NotificationService {
 
     @Override
